065c2b5240ce86a3b90ed0701f0a3a7a22c58294
Brady Bangasser <bradybbangasser@gmail.com>
2026-07-08 20:07:24 -0500

    Removed git verbosity

--- libghost/build.rs
+++ libghost/build.rs
@@ -1,5 +1,3 @@
-use std::process::Command;
-
 fn main() {
     prost_build::compile_protos(
         &["src/protocols/ghost/ghost.proto"],
@@ -12,24 +10,4 @@ fn main() {
         .build_server(false)
         .compile_protos(&["../proto/relay.proto"], &["../proto"])
         .unwrap();
-
-    let output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap().trim().to_string();
-
-    let branch_output = Command::new("git")
-        .args(["rev-parse", "--abbrev-ref", "HEAD"])
-        .output()
-        .unwrap();
-    let branch = String::from_utf8(branch_output.stdout)
-        .unwrap()
-        .trim()
-        .to_string();
-
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
-    println!("cargo:rustc-env=GIT_BRANCH={}", branch);
-
-    println!("cargo:rerun-if-changed=.git/HEAD");
 }
